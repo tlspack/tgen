5ca449ea136cc269cc2824df2b31175c4ecf9e68
tlspack <shpal360@gmail.com>
2020-07-14 02:20:54 +0000

    inital

--- cmds.py
+++ cmds.py
@@ -3,7 +3,7 @@ __author__ = 'Shirish Pal'
 import os
 
 def start_traffic (cfgid, result_tag):
-    os.system ( 'sudo docker run --name "{}-root" -it -d --volume=/root/rundir:/rundir tlspack/tgen tlspack.exe start "{}" "{}" /root/rundir 0'.format(cfgid, cfgid, result_tag) )
+    os.system ( 'sudo docker run --name "{}-root" -it -d --volume=/root/rundir:/rundir tgen tlspack.exe start "{}" "{}" /root/rundir 0'.format(cfgid, cfgid, result_tag) )
 
 def stop_traffic (cfgid):
-    os.system ( 'sudo docker run --rm -it --volume=/root/rundir:/rundir tlspack/tgen tlspack.exe stop "{}"'.format(cfgid) )
+    os.system ( 'sudo docker run --rm -it --volume=/root/rundir:/rundir tgen tlspack.exe stop "{}"'.format(cfgid) )
